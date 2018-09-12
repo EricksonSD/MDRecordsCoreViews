c7bcf1df1d6966b99f6b3b9a3e46ed5829c15d87
EricksonSD <esdomingo@yahoo.com>
2018-09-12 13:09:36 +0800

    Social History have smoking status, alcohol use status and drug use status

--- MedicalHistory/SocialHistoryEditor.php
+++ MedicalHistory/SocialHistoryEditor.php
@@ -27,7 +27,7 @@
                                     <th>SMOKING NOTES</th>
                                     <th>START DATE</th>
                                     <th>END DATE</th>
-                                    <th>TOTAL YEARS SMOKED</th>
+                                    <th>TOTAL YEARS</th>
                                       <th>ACTIONS</th>
                                 </tr>
                               
@@ -37,7 +37,7 @@
                      <details><summary><i class="DivHeaderText">ADD SMOKING STATUS</i> </summary>
                 SMOKING TYPE<br>
                 <input type="text" id="tbx_SmokingType" placeholder="Cigarettes / Vape / Tobacco / Others"/>  
-                <table width="100%"><tr><td width="250px">
+                <table width="100%"><tr><td width="340px">
                             SMOKER TYPE<br>
                             <input type="radio" name="SmokerType" value="Former smoker" checked>Former smoker
                             <br><input type="radio" name="SmokerType" value="Current everyday smoker">Current everyday smoker
@@ -106,12 +106,181 @@
         </div>
     
 
+<div class="DivTemplate"><p class="DivHeaderText">ALCOHOL USE STATUS</p><hr> 
+            <input type="checkbox" onclick="TogglerNonRadio('div_UsedAlcohol');" value="Never used alcohol">Never used alcohol
 
+            <div id="div_UsedAlcohol">
+                <input type="checkbox" onclick="TogglerNonRadio('div_KnownAlcoholUser');" value="Unknown if ever used alcohol">Unknown if ever used alcohol
+                <div id="div_KnownAlcoholUser">
+                    <br>
+                    <div class="dataDisplayer">
+                        <table width="100%" id="tbl_AlcoholUseStatus">
+                                                           <tr>
+                                    <th>ALCOHOL TYPE</th>
+                                    <th>DRINKER TYPE</th>
+                                    <th>FREQUENCY</th>
+                                    <th>ALCOHOL USE NOTES</th>
+                                    <th>START DATE</th>
+                                    <th>END DATE</th>
+                                    <th>TOTAL YEARS</th>
+                                      <th>ACTIONS</th>
+                                </tr>
+                              
 
-    <?php echo TogglerRadio('Former smoker', 'SmokerType', 'div_FormerSmoker') ?>
-    <?php echo TogglerRadio('Current everyday smoker', 'SmokerType', 'div_CurrentEverydaySmoker') ?>
-    <?php echo TogglerRadio('Current someday smoker', 'SmokerType', 'div_CurrentSomedaySmoker') ?>
-    <?php echo TogglerNonRadio() ?>
+                            </table>
+                    </div><br>
+                     <details><summary><i class="DivHeaderText">ADD ALCOHOL USE STATUS</i> </summary>
+                ALCOHOL TYPE<br>
+                <input type="text" id="tbx_AlcoholType" placeholder="Beer / Vodka / Gin / Hard liquor / Wine"/>  
+                <table width="100%"><tr><td width="340px">
+                            ALCOHOL TYPE<br>
+                            <input type="radio" name="AlcoholUserType" value="Former alcohol user" checked>Former alcohol user
+                            <br><input type="radio" name="AlcoholUserType" value="Current everyday alcohol user">Current everyday alcohol user
+                            <br><input type="radio" name="AlcoholUserType" value="Current someday alcohol user" >Current someday alcohol user
+                            <br><input type="radio" name="AlcoholUserType" value="Alcohol user, current status unknown" >Alcohol User, current status unknown
+                        <td>
+                            <!-- if former alcohol user -->
+                            <div id="div_FormerAlcoholUser">
+                                APPROXIMATE ALCOHOL USE START DATE<br><input id="tbx_FormerAlcoholUserStartDate" type="date" class="InputHalfWidth"/><br>
+                                APPROXIMATE ALCOHOL USE END DATE<br><input id="tbx_FormerAlcoholUserEndDate" type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF ALCOHOL USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                            <!-- if current everyday alcohol user -->
+                            <div id="div_CurrentEverydayAlcoholUser" style="display: none">
+                                APPROXIMATE ALCOHOL USE START DATE<br><input type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF ALCOHOL USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                            <!-- if current someday alcohol user -->
+                            <div id="div_CurrentSomedayAlcoholUser" style="display: none;">
+                                APPROXIMATE ALCOHOL USE START DATE<br><input type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF ALCOHOL USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <td >FREQUENCY<br><input id ="tbx_AlcoholUseFrequencyNumber" type="number" class="InputOneThirdWidth" placeholder="Number"/></td>
+                        <td class="threecolumns">UNIT<br><select id="sel_AlcoholUseUnit" class="InputOneThirdWidth" >
+                                <option value="Bottle(s)">Bottle(s)</option>
+                                <option value="Glass(es)">Glass(es)</option>
+                                <option value="ml.">ml.</option>
+                                <option value="oz.">oz.</option>
+                            </select>
+                        </td>
+                        <td class="threecolumns">PERIOD<br><input type="text" class="InputOneThirdWidth" id="tbx_AlcoholUsePeriod" placeholder="Per Day / Per Month / Thrice a week / Etc."></td>
+                    </tr>
+                </table>
+                ALCOHOL USE NOTES<br>
+                <textarea id="tbx_AlcoholUseNotes" rows="4" cols="50"></textarea>
+                <br><br>
+                <input type="button" value="ADD TO ALCOHOL USE STATUS LIST" onclick="AddToAlcoholUseStatus();"/>
+                     </details><br>
+                      <input type="checkbox" value="Alcohol Use Counseling" onclick="TogglerNonRadio('div_AlcoholUseCounseling')"> Advised for counseling to stop alcohol use
+                  <div id="div_AlcoholUseCounseling" style="display: none;">
+                                        <br>
+                      PATIENT RESPONSE<br>
+                      <input type="radio" name="Cessation Counseling Patient Response" value="Patient agreed" >Patient agreed<br>
+                  <input type="radio" name="Cessation Counseling Patient Response" value="Patient will think about it" >Patient will think about it<br>
+                   <input type="radio" name="Cessation Counseling Patient Response" value="Patient refused" >Patient refused<br>
+              
+                       
+                  </select><br>
+                       CESSATION COUNSELING NOTES<br>
+                <textarea id="tbx_AlcoholUseCessationCounselingNotes" rows="4" cols="50"></textarea>
+                  </div>
+                      
+                                   </div>
+            </div>
+        </div>
+
+         <div class="DivTemplate"><p class="DivHeaderText">DRUG USE STATUS</p><hr> 
+            <input type="checkbox" onclick="TogglerNonRadio('div_UsedDrug');" value="Never used drug">Never used drug
+
+            <div id="div_UsedDrug">
+                <input type="checkbox" onclick="TogglerNonRadio('div_KnownDrugUser');" value="Unknown if ever used drug">Unknown if ever used drug
+                <div id="div_KnownDrugUser">
+                    <br>
+                    <div class="dataDisplayer">
+                        <table width="100%" id="tbl_DrugUseStatus">
+                                                           <tr>
+                                    <th>DRUG TYPE</th>
+                                    <th>DRUG USER TYPE</th>
+                                    <th>FREQUENCY</th>
+                                    <th>DRUG USE NOTES</th>
+                                    <th>START DATE</th>
+                                    <th>END DATE</th>
+                                    <th>TOTAL YEARS</th>
+                                      <th>ACTIONS</th>
+                                </tr>
+                              
+
+                            </table>
+                    </div><br>
+                     <details><summary><i class="DivHeaderText">ADD DRUG USE STATUS</i> </summary>
+                DRUG TYPE<br>
+                <input type="text" id="tbx_DrugType" placeholder="Marijuana / Cocaine / Heroine / Metampethamine / PCP"/>  
+                <table width="100%"><tr><td width="340px">
+                            DRUG TYPE<br>
+                            <input type="radio" name="DrugUserType" value="Former drug user" checked>Former drug user
+                            <br><input type="radio" name="DrugUserType" value="Current everyday drug user">Current everyday drug user
+                            <br><input type="radio" name="DrugUserType" value="Current someday drug user" >Current someday drug user
+                            <br><input type="radio" name="DrugUserType" value="Drug user, current status unknown" >Drug User, current status unknown
+                        <td>
+                            <!-- if former drug user -->
+                            <div id="div_FormerDrugUser">
+                                APPROXIMATE DRUG USE START DATE<br><input id="tbx_FormerDrugUserStartDate" type="date" class="InputHalfWidth"/><br>
+                                APPROXIMATE DRUG USE END DATE<br><input id="tbx_FormerDrugUserEndDate" type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF DRUG USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                            <!-- if current everyday drug user -->
+                            <div id="div_CurrentEverydayDrugUser" style="display: none">
+                                APPROXIMATE DRUG USE START DATE<br><input type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF DRUG USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                            <!-- if current someday drug user -->
+                            <div id="div_CurrentSomedayDrugUser" style="display: none;">
+                                APPROXIMATE DRUG USE START DATE<br><input type="date" class="InputHalfWidth"/><br>
+                                TOTAL YEARS OF DRUG USAGE&emsp;<i class="DivHeaderText"><?php echo 'Auto-generated based on input' ?></i>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <td >FREQUENCY<br><input id ="tbx_DrugUseFrequencyNumber" type="number" class="InputOneThirdWidth" placeholder="Number"/></td>
+                        <td class="threecolumns">UNIT<br><select id="sel_DrugUseUnit" class="InputOneThirdWidth" >
+                                <option value="Bottle(s)">Bottle(s)</option>
+                                <option value="Glass(es)">Glass(es)</option>
+                                <option value="ml.">ml.</option>
+                                <option value="oz.">oz.</option>
+                            </select>
+                        </td>
+                        <td class="threecolumns">PERIOD<br><input type="text" class="InputOneThirdWidth" id="tbx_DrugUsePeriod" placeholder="Per Day / Per Month / Thrice a week / Etc."></td>
+                    </tr>
+                </table>
+                DRUG USE NOTES<br>
+                <textarea id="tbx_DrugUseNotes" rows="4" cols="50"></textarea>
+                <br><br>
+                <input type="button" value="ADD TO DRUG USE STATUS LIST" onclick="AddToDrugUseStatus();"/>
+                     </details><br>
+                      <input type="checkbox" value="Drug Use Counseling" onclick="TogglerNonRadio('div_DrugUseCounseling')"> Advised for counseling to stop drug use
+                  <div id="div_DrugUseCounseling" style="display: none;">
+                                        <br>
+                      PATIENT RESPONSE<br>
+                      <input type="radio" name="Cessation Counseling Patient Response" value="Patient agreed" >Patient agreed<br>
+                  <input type="radio" name="Cessation Counseling Patient Response" value="Patient will think about it" >Patient will think about it<br>
+                   <input type="radio" name="Cessation Counseling Patient Response" value="Patient refused" >Patient refused<br>
+              
+                       
+                  </select><br>
+                       CESSATION COUNSELING NOTES<br>
+                <textarea id="tbx_DrugUseCessationCounselingNotes" rows="4" cols="50"></textarea>
+                  </div>
+                      
+                                   </div>
+            </div>
+        </div>
 
 
     <div class="DivTemplate" id="div_Actions2"><p class="DivHeaderText">ACTIONS</p><hr>
--- MedicalHistory/SocialHistoryFunctions.php
+++ MedicalHistory/SocialHistoryFunctions.php
@@ -1,3 +1,18 @@
+   <?php echo TogglerNonRadio() ?>
+
+    <?php echo TogglerRadio('Former smoker', 'SmokerType', 'div_FormerSmoker') ?>
+    <?php echo TogglerRadio('Current everyday smoker', 'SmokerType', 'div_CurrentEverydaySmoker') ?>
+    <?php echo TogglerRadio('Current someday smoker', 'SmokerType', 'div_CurrentSomedaySmoker') ?>
+ 
+    <?php echo TogglerRadio('Former alcohol user', 'AlcoholUserType', 'div_FormerAlcoholUser') ?>
+    <?php echo TogglerRadio('Current everyday alcohol user', 'AlcoholUserType', 'div_CurrentEverydayAlcoholUser') ?>
+    <?php echo TogglerRadio('Current someday alcohol user', 'AlcoholUserType', 'div_CurrentSomedayAlcoholUser') ?>
+
+   <?php echo TogglerRadio('Former drug user', 'DrugUserType', 'div_FormerDrugUser') ?>
+    <?php echo TogglerRadio('Current everyday drug user', 'DrugUserType', 'div_CurrentEverydayDrugUser') ?>
+    <?php echo TogglerRadio('Current someday drug user', 'DrugUserType', 'div_CurrentSomedayDrugUser') ?>
+
+<!--Smoking script-->
 
 <script type="text/javascript">
     function AddToSmokingStatus() {
@@ -51,8 +66,130 @@
             seventhColumn.innerHTML = calculateTimeDiff(document.getElementById("tbx_FormerSmokerStartDate").value, document.getElementById("tbx_FormerSmokerEndDate").value);
             currentCell = currentRow.insertCell(-1);
             currentCell.appendChild(seventhColumn);
-        } else {
-            alert('OMG!');
-        }
+        } 
     }
 </script>
+
+<!--Alcohol use script-->
+
+
+<script type="text/javascript">
+    function AddToAlcoholUseStatus() {
+        var myTable = document.getElementById("tbl_AlcoholUseStatus");
+        var currentIndex = myTable.rows.length;
+        var currentRow = myTable.insertRow(currentIndex);
+
+        var firstColumn = document.createElement("i");
+        firstColumn.setAttribute("id", "lbl_AlcoholType" + currentIndex);
+        firstColumn.innerHTML = document.getElementById("tbx_AlcoholType").value;
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(firstColumn);
+
+        var secondColumn = document.createElement("i");
+        secondColumn.setAttribute("id", "lbl_AlcoholUserType" + currentIndex);
+        secondColumn.innerHTML = $('input[name="AlcoholUserType"]:checked').val();
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(secondColumn);
+
+        var thirdColumn = document.createElement("i");
+        thirdColumn.setAttribute("id", "lbl_Frequency" + currentIndex);
+  
+                thirdColumn.innerHTML = document.getElementById("tbx_AlcoholUseFrequencyNumber").value + " " + $('#sel_AlcoholUseUnit').find(":selected").text()
+                        + " " + document.getElementById("tbx_AlcoholUsePeriod").value;
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(thirdColumn);
+
+        var fourthColumn = document.createElement("i");
+        fourthColumn.setAttribute("id", "lbl_AlcoholUseNotes" + currentIndex);
+                  fourthColumn.innerHTML = $('textarea#tbx_AlcoholUseNotes').val();
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(fourthColumn);
+
+        var fifthColumn = document.createElement("i");
+        var sixthColumn = document.createElement("i");
+        var seventhColumn = document.createElement("i");
+
+        if ($('input[name="SmokerType"]:checked').val() === "Former smoker") {
+            fifthColumn.setAttribute("id", "lbl_FormerSmokerStartDate" + currentIndex);
+            fifthColumn.innerHTML = document.getElementById("tbx_FormerSmokerStartDate").value;
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(fifthColumn);
+            sixthColumn.setAttribute("id", "lbl_FormerSmokerEndDate" + currentIndex);
+            sixthColumn.innerHTML = document.getElementById("tbx_FormerSmokerEndDate").value;
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(sixthColumn);
+            seventhColumn.setAttribute("id", "lbl_FormerSmokerTotalYearsSmoked" + currentIndex);
+            seventhColumn.innerHTML = calculateTimeDiff(document.getElementById("tbx_FormerSmokerStartDate").value, document.getElementById("tbx_FormerSmokerEndDate").value);
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(seventhColumn);
+        } 
+    }
+</script>
+
+
+<!--Drug use script-->
+
+<script type="text/javascript">
+    function AddToDrugUseStatus() {
+        var myTable = document.getElementById("tbl_DrugUseStatus");
+        var currentIndex = myTable.rows.length;
+        var currentRow = myTable.insertRow(currentIndex);
+
+        var firstColumn = document.createElement("i");
+        firstColumn.setAttribute("id", "lbl_DrugType" + currentIndex);
+        firstColumn.innerHTML = document.getElementById("tbx_DrugType").value;
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(firstColumn);
+
+        var secondColumn = document.createElement("i");
+        secondColumn.setAttribute("id", "lbl_DrugUserType" + currentIndex);
+        secondColumn.innerHTML = $('input[name="DrugUserType"]:checked').val();
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(secondColumn);
+
+        var thirdColumn = document.createElement("i");
+        thirdColumn.setAttribute("id", "lbl_Frequency" + currentIndex);
+  
+                thirdColumn.innerHTML = document.getElementById("tbx_DrugUseFrequencyNumber").value + " " + $('#sel_DrugUseUnit').find(":selected").text()
+                        + " " + document.getElementById("tbx_DrugUsePeriod").value;
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(thirdColumn);
+
+        var fourthColumn = document.createElement("i");
+        fourthColumn.setAttribute("id", "lbl_DrugUseNotes" + currentIndex);
+                  fourthColumn.innerHTML = $('textarea#tbx_DrugUseNotes').val();
+
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(fourthColumn);
+
+        var fifthColumn = document.createElement("i");
+        var sixthColumn = document.createElement("i");
+        var seventhColumn = document.createElement("i");
+
+        if ($('input[name="SmokerType"]:checked').val() === "Former smoker") {
+            fifthColumn.setAttribute("id", "lbl_FormerSmokerStartDate" + currentIndex);
+            fifthColumn.innerHTML = document.getElementById("tbx_FormerSmokerStartDate").value;
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(fifthColumn);
+            sixthColumn.setAttribute("id", "lbl_FormerSmokerEndDate" + currentIndex);
+            sixthColumn.innerHTML = document.getElementById("tbx_FormerSmokerEndDate").value;
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(sixthColumn);
+            seventhColumn.setAttribute("id", "lbl_FormerSmokerTotalYearsSmoked" + currentIndex);
+            seventhColumn.innerHTML = calculateTimeDiff(document.getElementById("tbx_FormerSmokerStartDate").value, document.getElementById("tbx_FormerSmokerEndDate").value);
+            currentCell = currentRow.insertCell(-1);
+            currentCell.appendChild(seventhColumn);
+        } 
+    }
+</script>
+
+
+
+
