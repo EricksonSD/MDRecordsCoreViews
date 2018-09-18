2fb96d31f48fb06336bd9ac77d56e0b540258ff1
EricksonSD <esdomingo@yahoo.com>
2018-09-18 08:36:24 +0800

    Added family history and Hospital Admission; Removed IOS because it is not realated to my views

--- ios-php/includes/Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'iosdb');
- 
-define('USER_CREATED', 0);
-define('USER_ALREADY_EXIST', 1);
-define('USER_NOT_CREATED', 2);
